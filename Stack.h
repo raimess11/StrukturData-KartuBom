3a69fae6d437d3d398ef1ea15fa647542f881388
Regyyyy <rahma.sakti24@gmail.com>
2023-01-01 16:15:15 +0700

    Update Stack.h
    define stack komponent

--- Stack.h
+++ Stack.h
@@ -1,6 +1,21 @@
 #ifndef STACK_H_INCLUDED
 #define STACK_H_INCLUDED
 
+#include <iostream>
+using namespace std;
 
+typedef string infotype;
+
+struct stack {
+    infotype info[15];
+    int Top;
+};
+
+void createStack(stack &S);
+bool isEmpty(stack S);
+bool isFull(stack S);
+void push(stack &S,infotype x);
+string pop(stack &S);
+void printInfo(stack S);
 
 #endif // STACK_H_INCLUDED
