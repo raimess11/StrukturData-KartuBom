dfa0151b84c040e9ecdc367e787eedf25f9a7c8b
Regyyyy <rahma.sakti24@gmail.com>
2023-01-01 18:06:07 +0700

    Create ElementKartu.cpp

---
+++ ElementKartu.cpp
@@ -0,0 +1,11 @@
+//player
+void takeCard(player &CurrentPlayer, player &nextPlayer, bool &finish)
+/*{I.S. kartu pemain selanjutnya lebih dari 1
+F.S. kartu pemain selanjutnya diambil dan ditampung dalam variabel, jumlahKartu pemain selanjutnya berkurang 1. Jika kartu pemain selanjutnya sisa 1, maka permainan berakhir}*/
+{
+
+}
+void removeCard(addressHand &removedCard, player targetPlayer)
+{
+
+}
