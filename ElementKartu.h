12602d9ced3d1e7ac9ca0ad0a19c3d3e88502c67
Regyyyy <rahma.sakti24@gmail.com>
2023-01-01 15:57:12 +0700

    Update ElementKartu.h
    type definition

--- ElementKartu.h
+++ ElementKartu.h
@@ -1,5 +1,28 @@
 #ifndef ELEMENTKARTU_H_INCLUDED
 #define ELEMENTKARTU_H_INCLUDED
 
+//player
+struct player
+{
+    string namaPlayer;
+    hand deck;
+    int jumlahKartu;
+};
+
+//players
+typedef player infotypePlayer;
+typedef elementPlayer* addressPlayer;
+
+struct elementPlayer
+{
+    infotypePlayer info;
+    addressPlayer next;
+};
+
+struct listPlayer
+{
+    addressPlayer first;
+};
+
 
 #endif // ELEMENTKARTU_H_INCLUDED
